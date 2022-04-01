7b057e67cc4c428f3a18a9d18021cc57a2e9596e
Eduardo Carreiro <eduardo@wpcwebsolutions.com>
2022-04-01 09:13:01 -0300

    open close modal

--- js/eventos.js
+++ js/eventos.js
@@ -1,3 +1,17 @@
+const abrirModal = document.querySelectorAll(".btnreservar");
+const modal = document.getElementById("popupreserva");
+const concluirReserva = document.querySelector(".concluirreservar");
+const cancelarReserva = document.querySelector("#fechar");
+let nomeEvento = document.getElementById("eventoNome");
+let idEvento = "";
+
+var estilo = document.getElementsByClassName("load");
+
+let form = document.querySelector("form");
+let nome = document.querySelector("#nome");
+let email = document.querySelector("#email");
+let qtde = document.querySelector("#qtdIngressos");
+
 const exibirEventos = async () => {
 	var requestOptions = {
 		method: "GET",
@@ -15,10 +29,23 @@ const exibirEventos = async () => {
                     <h2>${contentEventos[index].name}</h2><h4> ${contentEventos[index].scheduled.substring(0, 10)}</h4>
                     <h4>${contentEventos[index].attractions}</h4>
                     <p>${contentEventos[index].description}</p>
-                    <a   href="#?id=${contentEventos[index]._id}" class="btn btn-primary res">reservar ingresso</a>
-                    </article>`;
+					<button onclick ="reservar('${contentEventos[index]._id}', 
+					'${contentEventos[index].name}')"
+					class="btn btn-primary botao-reservar">reservar ingresso</button>`;
 		let listaEventos = document.querySelector("#todososeventos");
 		listaEventos.innerHTML += eventosTable;
 	}
 };
 exibirEventos();
+
+let reservar = async (id, nome) => {
+	modal.style.display = "block";
+	nomeEvento.innerHTML = "Evento: " + nome;
+	var idEvento = id;
+	console.log(idEvento);
+};
+
+let cancelar = async (e) => {
+	e.preventdefault();
+	modal.style.display = "none";
+};
